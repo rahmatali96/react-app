544f2e140404d078196684b9c226e3a976d96850
Rahmat Ali <54480510+rahmatali96@users.noreply.github.com>
2024-09-03 12:42:23 +0530

    Fix build issue

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 // })
 
 export default defineConfig({
+  base: ".github/workflows/build.yml",
   server: {
     port: 3000, // Replace with your desired port number
   },
